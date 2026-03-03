e12ead5102d0356e1e783040f0c512bf5fd877b0
Torbjørn Lium <torben@lium.org>
2026-03-03 09:27:04 +0100

    Fix output file

--- src/xsoar_dependency_graph/exporter.py
+++ src/xsoar_dependency_graph/exporter.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 import networkx as nx
 
-SUPPORTED_OUTPUT_FORMATS = ["GML", "GraphML", "JSON"]
+SUPPORTED_OUTPUT_FORMATS = ["GML", "GraphML"]
 
 
 class Exporter:
     def __init__(self, graph: nx.Graph) -> None:
         self.graph = graph
+        print(graph)
 
     def export(self, output_path: Path, output_format: str) -> None:
         if output_format not in SUPPORTED_OUTPUT_FORMATS:
@@ -15,8 +16,10 @@ class Exporter:
             raise ValueError(msg)
 
         if output_format == "GML":
+            output_path = Path(output_path) / "output.gml"
             nx.write_gml(self.graph, output_path)
         elif output_format == "GraphML":
+            output_path = Path(output_path) / "output.graphml"
             nx.write_graphml(self.graph, output_path)
         else:
             msg = f"Invalid output format. Expected one of {','.join(SUPPORTED_OUTPUT_FORMATS)}"
